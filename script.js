2a886101c2bd3344d05e833e49341f8c11f2fe87
Abaz <Abaz95@live.co.uk>
2024-04-05 14:45:03 +0100

    Create player and computer score tracker.
    A point is added each time either the compuer or player wins.
    
    Player and computer choices are shown and remain after each round.

--- script.js
+++ script.js
@@ -6,62 +6,77 @@ let displayPlayerChoice = document.getElementById("displayPlayerChoice")
 let playerScore = document.getElementById("playerScore")
 let computerScore = document.getElementById("computerScore")
 
+
+
+// begin game on click
+const playerButtons = document.querySelectorAll("#rock, #paper, #scissors")
+playerButtons.forEach(playerButtons => playerButtons.addEventListener("click", play))
+
+function play (e) {
+    const playerChoice = e.target.id;
+    let computerChoice = getComputerChoice();
+    addPara(playerChoice, computerChoice); 
+
+    getWinner(playerChoice, computerChoice);
+}
+
+// display player and computer choices
+function addPara (playerChoice, computerChoice) {
+   
+   const playerPara = document.createElement("p");
+   const playerNode = document.createTextNode ("You chose: " + playerChoice);
+   playerPara.appendChild(playerNode);
+   const playerElement = document.getElementById("displayPlayerChoice");
+   playerElement.appendChild(playerPara);
+
+   const computerPara = document.createElement("p");
+   const ComputerNode = document.createTextNode ("Computer chose: " + computerChoice);
+   computerPara.appendChild(ComputerNode);
+   const computerElement = document.getElementById("displayComputerChoice");
+   computerElement.appendChild(computerPara)
+ };
+
+
 // Get computer's choice
 function getComputerChoice() {
     const arrOfChoices = ["rock", "paper", "scissors"]
     const randomNum = Math.floor(Math.random()*3)
     let computerChoice = arrOfChoices[randomNum]
-    displayComputerChoice.textContent = "Computer chose: " + computerChoice    
     return computerChoice
 }
 
-// Show player's choice on click
-const playerButtons = document.querySelectorAll(".playerButtons")
-
-playerButtons.forEach(playerButtons => playerButtons.addEventListener("click", play))
-
-function play (e) {
-    const playerChoice = e.target.id;
-    displayPlayerChoice.textContent = "You chose: " + playerChoice;
-
-    let computerChoice = getComputerChoice();
-
-    getWinner(playerChoice, computerChoice);
-}
-
-
 // Get game winner
 function getWinner(playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "rock") {
+        displayResult.textContent = "you tied! You both picked rock."
+    } else if (playerChoice === "paper" && computerChoice === "paper") {
+        displayResult.textContent = "you tied! You both picked paper."
+    } else if (playerChoice === "scissors" && computerChoice === "scissors") {
+        displayResult.textContent = "you tied! You both picked scissors."
 
-    console.log(playerChoice, computerChoice)
+    }  else if (playerChoice === "rock" && computerChoice === "scissors") {
+        displayResult.textContent = "you win!"
+        playerScore.textContent += "🐵"
 
-         if (playerChoice === "rock" && computerChoice === "rock") {
-            displayResult.textContent = "you tied! You both picked rock."
-
-        } else if (playerChoice === "paper" && computerChoice === "paper") {
-
-            displayResult.textContent = "you tied! You both picked paper."
-
-        } else if (playerChoice === "scissors" && computerChoice === "scissors") {
-            displayResult.textContent = "you tied! You both picked scissors."
-
-        }  else if (playerChoice === "rock" && computerChoice === "scissors") {
-            displayResult.textContent = "you win!"
-
-        } else if (playerChoice === "rock" && computerChoice === "paper") {
-            displayResult.textContent = "you looe!"
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        displayResult.textContent = "you loose!"
+        computerScore.textContent += "🤖"
             
-        } else if (playerChoice === "paper" && computerChoice === "rock") {
-           displayResult.textContent = "you win!"
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        displayResult.textContent = "you win!"
+        playerScore.textContent += "🐵"
             
-        } else if (playerChoice === "paper" && computerChoice === "scissors") {
-            displayResult.textContent = "you lose!"
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        displayResult.textContent = "you lose!"
+        computerScore.textContent += "🤖"
+    
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        displayResult.textContent = "you win!"
+        playerScore.textContent += "🐵"
 
-        } else if (playerChoice === "scissors" && computerChoice === "paper") {
-            displayResult.textContent = "you win!"
-
-        } else if (playerChoice === "scissors" && computerChoice === "rock") {
-            displayResult.textContent = "you lose!"
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        displayResult.textContent = "you lose!"
+        computerScore.textContent += "🤖"
     }
 }
 
