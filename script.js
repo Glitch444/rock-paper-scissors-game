383159db7a8e86ceb96d99c618f1ceba339f366f
Abaz <Abaz95@live.co.uk>
2024-06-24 14:22:39 +0100

    refactor code

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
-let displayResult = document.getElementById("displayResult");
-let displayComputerChoice = document.getElementById("displayComputerChoice");
-let displayPlayerChoice = document.getElementById("displayPlayerChoice");
-
-let playerScore = document.getElementById("playerScore");
-let computerScore = document.getElementById("computerScore");
-let ps = 0;
-let cs = 0;
-
-
-// begin game on click
 const playerButtons = document.querySelectorAll("#rock, #paper, #scissors");
-playerButtons.forEach( (b) => b.addEventListener("click", play));
+const displayPlayerChoice = document.getElementById("display-player-choice");
+const displayComputerChoice = document.getElementById("display-computer-choice");
+const displayResult = document.getElementById("display-result");
 
-function play (e) {
-    const playerChoice = e.target.id;
+const displayPlayerScore = document.getElementById("display-player-score");
+const displayComputerScore = document.getElementById("display-computer-score");
+
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice = null;
+
+// get player's choice
+playerButtons.forEach(function (button) {
+    button.addEventListener("click", function() {
+        playerChoice = button.id;
+        playGame(playerChoice);
+    });
+});
+
+
+
+// game
+function playGame (playerChoice) {    
     const computerChoice = getComputerChoice();
 
-    addPara(playerChoice, computerChoice); 
-    getWinner(playerChoice, computerChoice);
+    displayPlayerChoice.textContent = `🐵 YOU: ${playerChoice}`;
+    displayComputerChoice.textContent = `💻 COMPUTER: ${computerChoice}`;
+
+    if (playerChoice === computerChoice) {
+
+        displayResult.textContent = "IT'S A TIE!"; 
+    } 
     
+    else if (
+        (playerChoice === "rock" && computerChoice === "scissors")||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        displayResult.textContent = "YOU WIN!";
+        playerScore += 1;
+        displayPlayerScore.textContent = playerScore;
+       
+    } else {
+        displayResult.textContent = "YOU LOSE!";
+        computerScore += 1;
+        displayComputerScore.textContent = computerScore;
+    }
+
+    let result = displayResult.textContent
+    displayResult.classList.remove("greenText", "redText");
+
+    switch (result) {
+        case "YOU WIN!":
+            displayResult.classList.add("greenText");
+            break;
+        case "YOU LOSE!":
+            displayResult.classList.add("redText")
+            break;
+
+    }
+
 }
 
-// display player and computer choices
-function addPara (playerChoice, computerChoice) {
-   
-   const playerPara = document.createElement("p");
-   const playerNode = document.createTextNode ("You chose: " + playerChoice);
-   playerPara.appendChild(playerNode);
-   const playerElement = document.getElementById("displayPlayerChoice");
-   playerElement.appendChild(playerPara);
-
-   const computerPara = document.createElement("p");
-   const ComputerNode = document.createTextNode ("Computer chose: " + computerChoice);
-   computerPara.appendChild(ComputerNode);
-   const computerElement = document.getElementById("displayComputerChoice");
-   computerElement.appendChild(computerPara);
- };
-
-
-// Get computer's choice
+// get computer's choice
 function getComputerChoice() {
     const arrOfChoices = ["rock", "paper", "scissors"];
     const randomNum = Math.floor(Math.random()*3);
@@ -46,62 +70,18 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-// Get game winner
-function getWinner(playerChoice, computerChoice) {
-    if (playerChoice === "rock" && computerChoice === "rock") {
-        displayResult.textContent = "It's a tie!"
-    } else if (playerChoice === "paper" && computerChoice === "paper") {
-        displayResult.textContent = "It's a tie!"
-    } else if (playerChoice === "scissors" && computerChoice === "scissors") {
-        displayResult.textContent = "It's a tie!"
-
-
-    }  else if (playerChoice === "rock" && computerChoice === "scissors") {
-        displayResult.textContent = "You won this round!";
-        ps += 1;
-        playerScore.textContent += "🐵";
-       
-    } else if (playerChoice === "rock" && computerChoice === "paper") {
-        displayResult.textContent = "You lost this round!"
-        cs += 1;
-        computerScore.textContent += "🤖";
-            
-    } else if (playerChoice === "paper" && computerChoice === "rock") {
-        displayResult.textContent = "You won this round!"
-        ps += 1;
-        playerScore.textContent += "🐵";
-        
-
-    } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        displayResult.textContent = "You lost this round!"
-        cs += 1;
-        computerScore.textContent += "🤖";
-    
-    } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        displayResult.textContent = "You won this round!"
-        ps += 1;
-        playerScore.textContent += "🐵";
-        
-    } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        displayResult.textContent = "You lost this round!"
-        cs += 1;
-        computerScore.textContent += "🤖";
-    }
-
-    winningMessage ()
-}
 
 
 
-let delayInMilliseconds = 600
+// let delayInMilliseconds = 600
 
-function winningMessage () {
-    setTimeout(function(){
-    if (ps === 3) {
-        alert("YOU WON THE GAME! Refresh page to have another go!")
-    } else if (cs === 3) {
-        alert ("YOU LOST THE GAME! Refresh page to have another go!")
-    }
-}, delayInMilliseconds);
+// function winningMessage () {
+//     setTimeout(function(){
+//     if (ps === 3) {
+//         alert("YOU WON THE GAME! Refresh page to have another go!")
+//     } else if (cs === 3) {
+//         alert ("YOU LOST THE GAME! Refresh page to have another go!")
+//     }
+// }, delayInMilliseconds);
 
-}
+// }
